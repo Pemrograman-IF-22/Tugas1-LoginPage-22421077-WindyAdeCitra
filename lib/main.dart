43fdfde55ac01b7e25727a57069ae2bd902fbc08
windy0354 <windyadecitra860@gmail.com>
2025-03-18 11:36:43 +0700

    membuat dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Login Page',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login Page'),
-        ))
+      home: LoginPage()
     );
   }
 }
